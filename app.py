4afce82cf599cd478e2b88dd58971a8824c2f046
Hema Veeradhi <hemav@bu.edu>
2018-06-27 14:57:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,19 +26,6 @@ def hello_world():
     REQUESTS.labels(method='GET', endpoint="/", status_code=200).inc()  # Increment the counter
     return 'Hello, World!'
 
-# Note I'm intentionally failing occasionally to simulate a flakey service.
-@app.route('/slow')
-@TIMINGS.time()
-@IN_PROGRESS.track_inprogress()
-def slow_request():
-    v = random.expovariate(1.0 / 1.3)
-    if v > 3:
-        REQUESTS.labels(method='GET', endpoint="/slow", status_code=500).inc()
-        abort(500)
-    time.sleep(v)
-    REQUESTS.labels(method='GET', endpoint="/slow", status_code=200).inc()
-    return render_template_string('<h1>Wow, that took {{v}} s!</h1>', v=v)
-
 
 @app.route('/hello/<name>')
 @IN_PROGRESS.track_inprogress()
