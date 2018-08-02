312f3f56601cfd4ba6108b31362e8097a46966e7
Anand Sanmukhani <asanmukhredhat.com>
2018-08-02 14:03:20 -0400

    Make datetime not constant, so requeest datetime is the actual current time

--- servicemetrics.py
+++ servicemetrics.py
@@ -82,14 +82,10 @@ print(current_time)
 
 #converting to np.int64 type
 # current_time = np.int64(current_time)
-#Find the index matching with the current timestamp
-index = data.index.get_loc(current_time, method='nearest')
 
-print("The matching index found:", index, "nearest_timestamp is: ", data.iloc[[index]])
-#Set the Gauge with the predicted values of the index found
-PREDICTED_VALUES.labels(value_type='yhat').set(yhat[index])
-PREDICTED_VALUES.labels(value_type='yhat_upper').set(yhat_upper[index])
-PREDICTED_VALUES.labels(value_type='yhat_lower').set(yhat_lower[index])
+
+
+
 
 
 
@@ -121,7 +117,17 @@ def countpkg():
 
 @app.route('/metrics')
 def metrics():
-	return generate_latest(REGISTRY)
+    #Find the index matching with the current timestamp
+    global data
+    index = data.index.get_loc(datetime.now(), method='nearest')
+
+    print("The matching index found:", index, "nearest_timestamp is: ", data.iloc[[index]])
+    #Set the Gauge with the predicted values of the index found
+    
+    PREDICTED_VALUES.labels(value_type='yhat').set(yhat[index])
+    PREDICTED_VALUES.labels(value_type='yhat_upper').set(yhat_upper[index])
+    PREDICTED_VALUES.labels(value_type='yhat_lower').set(yhat_lower[index])
+    return generate_latest(REGISTRY)
 
 @app.route('/prometheus')
 @IN_PROGRESS.track_inprogress()
